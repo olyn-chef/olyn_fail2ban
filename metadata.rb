0dd66a17068c93360992e47e9e3bd900c5f0c2fd
Scott Richardson <dev@grogwood.com>
2019-12-16 10:38:37 -0800

    Package imported

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name 'olyn_fail2ban'
+maintainer 'Scott Richardson'
+maintainer_email 'dev@grogwood.com'
+chef_version '~> 15'
+license 'GPL-3.0'
+supports 'debian', '>= 10'
+source_url 'https://gitlab.com/olyn/olyn_fail2ban'
+description 'Installs and configures fail2ban'
+version '1.0.0'
+
+provides 'olyn_fail2ban::default'
+recipe 'olyn_fail2ban::default', 'Installs and configures fail2ban'
+
+depends 'fail2ban', '~> 6'
